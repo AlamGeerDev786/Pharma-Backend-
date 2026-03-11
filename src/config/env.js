19c32741e02192961112b2d1f9c2fad9935ae765
Alamgeer <khanalamgeer996@gmail.com>
2026-03-11 12:43:42 +0500

    CHNAGE BASEURL

--- src/config/env.js
+++ src/config/env.js
@@ -7,7 +7,8 @@ export const env = {
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
-  FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
+  // FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
+ FRONTEND_URL: process.env.FRONTEND_URL || 'https://pharma-frontend-eight.vercel.app/', 
 
   // SMTP
   SMTP_HOST: process.env.SMTP_HOST,
