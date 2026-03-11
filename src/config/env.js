7ab66e5d05540e62bfdd416f49edc95a488ac680
Alamgeer <khanalamgeer996@gmail.com>
2026-03-11 12:56:13 +0500

    change frontendUrl

--- src/config/env.js
+++ src/config/env.js
@@ -8,7 +8,7 @@ export const env = {
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
   // FRONTEND_URL: process.env.FRONTEND_URL || 'http://localhost:5173',
- FRONTEND_URL: process.env.FRONTEND_URL || 'https://pharma-frontend-eight.vercel.app/', 
+ FRONTEND_URL: process.env.FRONTEND_URL || 'https://pharma-frontend-eight.vercel.app',
 
   // SMTP
   SMTP_HOST: process.env.SMTP_HOST,
